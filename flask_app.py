8ec6420d07a45cac8f6d6473730fdaff7d60eadc
Carlos Gonzalez <carlos.gonzalez.valderrama@gmail.com>
2019-04-27 17:38:00 +0000

    Añadida conexión a base de datos

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,25 @@
 
 # A very simple Flask Hello World app for you to get started with...
-
+import yaml
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+with open("/home/cgonzalezv/mysite/config.yaml", 'r') as config_file:
+    CONFIG = yaml.safe_load(config_file)
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=CONFIG["username"],
+    password=CONFIG["password"],
+    hostname=CONFIG["hostname"],
+    databasename=CONFIG["databasename"]
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
